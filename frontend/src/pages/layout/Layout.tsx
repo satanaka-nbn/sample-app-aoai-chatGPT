6f8a6c695cfc3bb26eb2fb2e9bf128e0d4842b31
NBN\ueda <jumpbasket36@gmail.com>
2023-10-18 11:41:03 +0900

    Azure AIという文字列をMetele AIに変更

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -82,7 +82,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>Azure AI</h1>
+                            <h1 className={styles.headerTitle}>Metele AI</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
